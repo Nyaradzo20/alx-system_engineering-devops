784be36dba112e17303769401c99046954648092
Nyaradzo20 <nchinyati20@gmail.com>
2022-09-16 16:08:33 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating new file
+file { '/tmpschool':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+  }
